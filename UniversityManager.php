b7fbf72eb6386a4bcee41b5ee91e3f30085b17a1
Xavier Frehner <xavierfrehner@Xaviers-MBP.hitronhub.home>
2020-11-30 22:21:27 -0700

    basic shell with uni command options

--- UniversityManager.php
+++ UniversityManager.php
@@ -2,13 +2,29 @@
 require_once "Student.php";
 require_once "Dao.php";
 
-fwrite(STDOUT, 'What is your first name?');
-$first=fgets(STDIN);
-$first=str_replace("\n",'',$first);
-fwrite(STDOUT, 'What is your last name?');
-$last=fgets(STDIN);
-$last=str_replace("\n",'',$last);
-$student = new Student($first, $last);
-fwrite(STDOUT, $student->getUser());
 $dao = new Dao();
-print_r($dao->getStudents());
+
+$val='';
+fwrite(STDOUT,"What would you like to do?\n");
+fwrite(STDOUT,"a.) Create a class\n");
+fwrite(STDOUT,"b.) Delete a class\n");
+fwrite(STDOUT,"c.) Enroll a student\n");
+fwrite(STDOUT,"d.) Drop a student\n");
+    
+while($val!="exit\n") {
+
+    fwrite(STDOUT, "\$ ");
+    $val=fgets(STDIN);
+    if($val=="a\n") {
+        fwrite(STDOUT,"Create a class\n");
+    } else if($val=="b\n") {
+        fwrite(STDOUT,"Delete a class\n");
+    } else if($val=="c\n") {
+        fwrite(STDOUT,"Enroll a student\n");
+    } else if($val=="d\n") {
+        fwrite(STDOUT,"Drop a student\n");
+    } else if($val!="exit\n"){
+        $val=str_replace("\n","",$val);
+        fwrite(STDOUT,"$val is an invalid option, try again\n");
+    }
+}
